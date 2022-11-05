f36a5c7b0674a9f07f45a8b01258389614961a9c
Sooya <52819764+sooya12@users.noreply.github.com>
2022-11-05 17:52:00 +0900

    햄버거 만들기 / 문자열, StringBuilder

---
+++ Programmers/Solution_P햄버거만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i : ingredient) {
+            sb.append(i);
+            
+            if(sb.length() > 3 && sb.substring(sb.length () - 4).equals("1231")) {
+                answer++;
+                sb.delete(sb.length() - 4, sb.length());
+            }
+        }
+        
+        return answer;
+    }
+}
